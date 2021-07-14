62a25acd3b11b00d4664a194fd04c7c8bc285db8
Tautvydas Žukauskas <tautvydaszuk@gmail.com>
2021-07-15 02:35:14 +0300

    update grid creation to work with 1-100 cells

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let gridCells = 16;
+//Grid creation
+let gridCells = 4;
 let gridSquares = gridCells * gridCells;
 const gridContainer = document.querySelector("#gridContainer");
 
@@ -8,10 +9,13 @@ for (let i = 0; i < gridSquares; i++) {
   gridContainer.appendChild(gridItem);
 };
 
+document.documentElement.style.setProperty("--gridcolumns", gridCells);
+document.documentElement.style.setProperty("--gridrows", gridCells);
+
+// Hover effect
 const itemHover = document.querySelectorAll(".gridCell")
 for(let j = 0; j < itemHover.length; j++){
-
-itemHover[j].addEventListener("mouseover", function (e) {
-  e.target.setAttribute("style", "background-color:red;");
-});
+    itemHover[j].addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = "red";
+  });
 };
