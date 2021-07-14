2b0c83005fb8dc16f6b36e048f315e52dc6125ba
Tautvydas Žukauskas <tautvydaszuk@gmail.com>
2021-07-14 04:40:31 +0300

    add color on hover

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let gridCells = 16;
 let gridSquares = gridCells * gridCells;
 const gridContainer = document.querySelector("#gridContainer");
-  
-for (let i = 0; i < gridSquares; i++) {
-  const gridItem = document.createElement("div");
-  gridItem.classList.add("gridCell")
 
+for (let i = 0; i < gridSquares; i++) {
+  const gridItem = document.createElement("div");  
+  gridItem.classList.add("gridCell")
   gridContainer.appendChild(gridItem);
 };
 
+const itemHover = document.querySelectorAll(".gridCell")
+for(let j = 0; j < itemHover.length; j++){
+
+itemHover[j].addEventListener("mouseover", function (e) {
+  e.target.setAttribute("style", "background-color:red;");
+});
+};
