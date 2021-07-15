4266e3a20231e4ce165eee0e520fd4c1e06f04f8
Tautvydas Žukauskas <tautvydaszuk@gmail.com>
2021-07-16 00:42:53 +0300

    add button for new grid and other minor changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Grid creation
-let gridCells = 4;
+let gridCells = 16;
 let gridSquares = gridCells * gridCells;
 const gridContainer = document.querySelector("#gridContainer");
 
@@ -9,13 +9,38 @@ for (let i = 0; i < gridSquares; i++) {
   gridContainer.appendChild(gridItem);
 };
 
+const newGrid = document.querySelector("#newGrid");
+newGrid.addEventListener("click", Grid);
+
+
 document.documentElement.style.setProperty("--gridcolumns", gridCells);
 document.documentElement.style.setProperty("--gridrows", gridCells);
 
 // Hover effect
 const itemHover = document.querySelectorAll(".gridCell")
 for(let j = 0; j < itemHover.length; j++){
-    itemHover[j].addEventListener("mouseover", function (e) {
-      e.target.style.backgroundColor = "red";
+    itemHover[j].addEventListener("mousemove", function (e) {
+      e.target.style.backgroundColor = "black";
   });
 };
+
+
+function Grid() {
+  gridCells = prompt("Choose grid size between 1 and 100.")
+  gridSquares = gridCells * gridCells;
+  for (let i = 0; i < gridSquares; i++) {
+    const gridItem = document.createElement("div");  
+    gridItem.classList.add("gridCell")
+    gridContainer.appendChild(gridItem);
+  };
+  document.documentElement.style.setProperty("--gridcolumns", gridCells);
+  document.documentElement.style.setProperty("--gridrows", gridCells);
+
+  const itemHover = document.querySelectorAll(".gridCell")
+for(let j = 0; j < itemHover.length; j++){
+  itemHover[j].style.backgroundColor = "white";
+  itemHover[j].addEventListener("mousemove", function (e) {
+      e.target.style.backgroundColor = "black";
+  });
+};
+}
