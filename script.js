86a072a8227edcfde84af8e4531d667998dca7e7
Tautvydas Žukauskas <tautvydaszuk@gmail.com>
2021-07-16 01:09:36 +0300

    add grid clear button and some basic css design

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const gridContainer = document.querySelector("#gridContainer");
 
 for (let i = 0; i < gridSquares; i++) {
   const gridItem = document.createElement("div");  
-  gridItem.classList.add("gridCell")
+  gridItem.classList.add("gridCell");
   gridContainer.appendChild(gridItem);
 };
 
@@ -13,39 +13,51 @@ for (let i = 0; i < gridSquares; i++) {
 const newGrid = document.querySelector("#newGrid");
 newGrid.addEventListener("click", Grid);
 
+// event listener for clear Grid button
+const clearGrid = document.querySelector("#clearGrid");
+clearGrid.addEventListener("click", clean);
+
 //Setting css variables
 document.documentElement.style.setProperty("--gridcolumns", gridCells);
 document.documentElement.style.setProperty("--gridrows", gridCells);
 
 // Hover effect
-const itemHover = document.querySelectorAll(".gridCell")
+const itemHover = document.querySelectorAll(".gridCell");
 
 for(let j = 0; j < itemHover.length; j++){
     itemHover[j].addEventListener("mousemove", function (e) {
       e.target.style.backgroundColor = "black";
   });
 };
+// Clean grid function
 
+function clean() {
+  const itemHover = document.querySelectorAll(".gridCell");
+
+  for(let j = 0; j < itemHover.length; j++){
+  itemHover[j].style.backgroundColor = "white";}
+
+}
 //New grid function
 function Grid() {
-  gridCells = prompt("Choose grid size between 1 and 100.")
+  gridCells = prompt("Choose grid size between 1 and 100.");
 
   while (gridCells < 1 || gridCells > 100 ){
-    gridCells = prompt("Choose grid size between 1 and 100.")
+    gridCells = prompt("Choose grid size between 1 and 100.");
   }
 
   gridSquares = gridCells * gridCells;
 
   for (let i = 0; i < gridSquares; i++) {
     const gridItem = document.createElement("div");  
-    gridItem.classList.add("gridCell")
+    gridItem.classList.add("gridCell");
     gridContainer.appendChild(gridItem);
   };
 
   document.documentElement.style.setProperty("--gridcolumns", gridCells);
   document.documentElement.style.setProperty("--gridrows", gridCells);
 
-  const itemHover = document.querySelectorAll(".gridCell")
+  const itemHover = document.querySelectorAll(".gridCell");
 
   for(let j = 0; j < itemHover.length; j++){
   itemHover[j].style.backgroundColor = "white";
